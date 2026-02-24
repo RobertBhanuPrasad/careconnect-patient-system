c32acb22a1291a00bedbcf709e4bfeff8f2db4b4
Robert Bhanu Prasad <bhanu.robert@artofliving.org>
2026-02-24 16:08:37 +0530

    feat:patient flow is done and admin flow otp checking using the static passkey is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home({searchParams}: SearchParamProps) {
+  const { admin: _admin } = await searchParams
+  console.log(_admin, "adminbhanu")
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP Verification */}
+      {_admin && <PasskeyModal/>}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -15,11 +18,11 @@ export default function Home() {
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
-        </div>
-        <PatientForm/>
-        <div className="text-14-regular m-20 flex justify-between">
-          <p className="justify-items-end text-dark-600 xl:text-left">© 2026 CarePulse</p>
-          <Link href="/?admin=true" className="text-green-500">Admin</Link>
+          <PatientForm/>
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">© 2026 CarePulse</p>
+            <Link href="/?admin=true" className="text-green-500">Admin</Link>
+          </div>
         </div>
       </section>
       <Image
